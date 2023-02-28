ee4ccf67b80cd1a380069ba90edb3c14331688d2
Femi Olowonefa <feminefa@gmail.com>
2023-02-28 15:38:56 -0500

    added this wallet creation endpoint

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { call, callAsync, download } from './helpers/utils';
 import bodyParser from 'body-parser';
 import { nanoid } from 'nanoid';
 import { inscribe } from './service/inscribe';
+import { createWallet } from './service/create-wallet';
 const app = express();
 
 const jsonParser = bodyParser.json();
@@ -15,75 +16,8 @@ app.get('/',  (req: Request, res: Response) => {
   res.send('Hello World');
 });
 app.post('/inscribe', jsonParser, inscribe);
-app.post('/create-descriptors', jsonParser, async function (req, res) {
-  const { tr, wallet } = req.body;
-  try {
-    const createWallet = await callAsync(
-      'bitcoin-cli',
-      'createwallet',
-      [
-        `wallet_name=${wallet}`,
-        'blank=true',
-        'disable_private_keys=true',
-        'descriptors=true',
-      ],
-      ['named']
-    ).catch((e) => {
-      throw e;
-    });
-  } catch (e) {
-    if (e.message.includes('error') && !e.message.includes('exist')) {
-      return res.status(500).send(e);
-    }
-  }
-  await call(
-    'bitcoin-cli',
-    'getdescriptorinfo',
-    [`"tr(${tr}/0/*)"`],
-    [],
-    async (result1: any) => {
-      await call(
-        'bitcoin-cli',
-        'getdescriptorinfo',
-        [`"tr(${tr}/1/*)"`],
-        [],
-        async (result2: any) => {
-          const template = [
-            {
-              desc: '',
-              timestamp: 'now',
-              active: true,
-              internal: true,
-              range: [0, 999],
-              next: 0,
-            },
-            {
-              desc: '',
-              timestamp: 'now',
-              active: true,
-              internal: false,
-              range: [0, 999],
-              next: 0,
-            },
-          ];
-          template[0].desc = JSON.parse(result1.result).descriptor;
-          template[1].desc = JSON.parse(result2.result).descriptor;
-
-          await call(
-            'bitcoin-cli',
-            'importdescriptors',
-            [`'${JSON.stringify(template).replace(/'/g, `'\\''`)}'`],
-            [{ rpcwallet: wallet }],
-            async (result3: any) => {
-              res.status(result3.success ? 200 : 500).send(template);
-            }
-          );
-        }
-      );
-      // res.status(result.status).send(result);
-    }
-  );
-});
+app.post('/create-wallet', jsonParser, createWallet);
+  
 app.get('/:command/:action', async function (req, res) {
   console.log(req.params, req.query.params);
   await call(
---
+++ src/service/create-wallet.ts
@@ -0,0 +1,72 @@
+import {Request, Response} from 'express';
+import { call, callAsync, download, toFile } from '../helpers/utils';
+
+export const createWallet = async (req: Request, res: Response) => {
+    const { tr, name } = req.body;
+    try {
+      const createWallet = await callAsync(
+        'bitcoin-cli',
+        'createwallet',
+        [
+          `wallet_name=${name}`,
+          'blank=true',
+          'disable_private_keys=true',
+          'descriptors=true',
+        ],
+        ['named']
+      ).catch((e) => {
+        throw e;
+      });
+    } catch (e) {
+      if (e.message.includes('error') && !e.message.includes('exist')) {
+        return res.status(500).send(e);
+      }
+    }
+    await call(
+      'bitcoin-cli',
+      'getdescriptorinfo',
+      [`"tr(${tr}/0/*)"`],
+      [],
+      async (result1: any) => {
+        await call(
+          'bitcoin-cli',
+          'getdescriptorinfo',
+          [`"tr(${tr}/1/*)"`],
+          [],
+          async (result2: any) => {
+            const template = [
+              {
+                desc: '',
+                timestamp: 'now',
+                active: true,
+                internal: true,
+                range: [0, 999],
+                next: 0,
+              },
+              {
+                desc: '',
+                timestamp: 'now',
+                active: true,
+                internal: false,
+                range: [0, 999],
+                next: 0,
+              },
+            ];
+            template[0].desc = JSON.parse(result1.result).descriptor;
+            template[1].desc = JSON.parse(result2.result).descriptor;
+  
+            await call(
+              'bitcoin-cli',
+              'importdescriptors',
+              [`'${JSON.stringify(template).replace(/'/g, `'\\''`)}'`],
+              [{ rpcwallet: name }],
+              async (result3: any) => {
+                res.status(result3.success ? 200 : 500).send(template);
+              }
+            );
+          }
+        );
+        // res.status(result.status).send(result);
+      }
+    );
+};
