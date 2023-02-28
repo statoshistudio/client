7b56e56cbec8c15ef3d8ad807e471337519c0848
Femi Olowonefa <feminefa@gmail.com>
2023-02-28 05:23:01 -0500

    added npm run all

--- src/service/inscribe.ts
+++ src/service/inscribe.ts
@@ -22,7 +22,7 @@ try{
         throw e;
       });
       // console.log('CREATED_WALLET_RESPONSE', JSON.parse(String(createWallet)))
-
+console.log( createWallet);
     res.status(200).send(JSON.parse(createWallet));
 }catch(e) {
     res.status(500).send(e.message)
